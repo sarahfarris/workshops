977f7c06bde61f7c1c8bf2bdd9398995046f6d52
sarahfarris <sarahfarris45@gmail.com>
2025-05-08 14:22:27 -0700

    Phase 1

--- dealership_workshop/Car_Dealership.java
+++ dealership_workshop/Car_Dealership.java
@@ -1,14 +1,77 @@
 package dealership_workshop;
 
-public class Car_Dealership {
+import java.util.ArrayList;
 
+public class Car_Dealership {
     // create a file writer to input cars
     // be able to search by different properties within file
     // list all cars from file
     // add and remove cars
-
     // this class will contain getters for search functions
     // and add/remove cars to the file
-
     // will hold info about the dealership (name, address) and maintain a list of vehicles. since it has a list of vehicles, it will also have the methods that search the list for matching vehicles as well as add/remove vehicles
+    //will need getters and setters for every field except inventory
+    String name;
+    String address;
+    String phoneNr;
+
+    ArrayList<Vehicle> inventory; //just the declaration, not instantiation
+    // the array list is now part of the parameter
+    public Car_Dealership(String name, String address, String phoneNr, ArrayList<Vehicle> inventory) {
+        this.name = name;
+        this.address = address;
+        this.phoneNr = phoneNr;
+        this.inventory = new ArrayList<>(inventory);
+    }
+
+    //do I need to add the line splitter for the file here?
+    //then if that is the case, do I need a toString() method here?
+
+    public String getName(){
+        return name;
+    }
+    public String getAddress(){
+        return address;
+    }
+    public String getPhoneNr(){
+        return phoneNr;
+    }
+
+    public Vehicle getVehiclesByPrice(double min, double max){
+        ArrayList<Vehicle> inventory;
+        // create user input variables for min price and max price, and make it a range to search through
+        return null;
+    }
+    public Vehicle getVehiclesByMakeModel(String make, String model){
+        ArrayList<Vehicle> inventory;
+        //create user input variables that searches by make and model, similar to how we did it in ledgerApp for search by vendor
+        return null;
+    }
+    public Vehicle getVehiclesByYear(double min, double max){
+        ArrayList<Vehicle> inventory;
+        //create user input variables for min/max year, make it a range to search through
+        return null;
+    }
+    public Vehicle getVehiclesByColor(String color){
+        ArrayList<Vehicle> inventory;
+        return null;
+    }
+    public Vehicle getVehiclesByMileage(double min, double max){
+        ArrayList<Vehicle> inventory;
+        return null;
+    }
+    public Vehicle getVehiclesByType(String vehicleType){
+        ArrayList<Vehicle> inventory;
+        return null;
+    }
+    public Vehicle getAllVehicles(){
+        ArrayList<Vehicle> inventory;
+        return null;
+    }
+    public Vehicle addVehicle(){
+        return null;
+    }
+    public Vehicle removeVehicle(){
+        return null;
+    }
 }
--- dealership_workshop/Vehicle.java
+++ dealership_workshop/Vehicle.java
@@ -1,5 +1,25 @@
 package dealership_workshop;
 
 public class Vehicle {
-    // will hold info about a specific vehicle
+    // will hold info about a specific vehicle\
+    //will hold getters and setters for every field
+    int vin;
+    int year;
+    String make;
+    String model;
+    String vehicleType;
+    String color;
+    int odometer;
+    double price;
+
+    public Vehicle(int vin, int year, String make, String model, String vehicleType, String color, int odometer, double price) {
+        this.vin = vin;
+        this.year = year;
+        this.make = make;
+        this.model = model;
+        this.vehicleType = vehicleType;
+        this.color = color;
+        this.odometer = odometer;
+        this.price = price;
+    }
 }
